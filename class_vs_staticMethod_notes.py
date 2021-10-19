db4574662aa99377c7119094357d9ef0d4e95518
mark <markpackham1@gmail.com>
2021-10-19 10:48:58 +0100

    add class Vs instance static method notes
    https://youtu.be/Ej_02ICOIgs?t=4406

---
+++ class_vs_staticMethod_notes.py
@@ -0,0 +1,31 @@
+# When to use class methods and when to use static methods ?
+
+class Item:
+    @staticmethod
+    def is_integer():
+        '''
+        This should do something that has a relationship
+        with the class, but not something that must be unique
+        per instance!
+        '''
+    @classmethod
+    def instantiate_from_something(cls):
+        '''
+        This should also do something that has a relationship
+        with the class, but usually, those are used to
+        manipulate different structures of data to instantiate
+        objects, like we have done with CSV.
+        '''
+
+# THE ONLY DIFFERENCE BETWEEN THOSE:
+# Static methods are not passing the object reference as the first argument in the background!
+
+
+# However, those could be also called from instances (although I have no idea why you'd want to do that)
+item1 = Item()
+item1.is_integer()
+item1.instantiate_from_something()
+
+# It's better practice to call both from the Class and not the Instance
+Item.is_integer()
+Item.instantiate_from_something()
