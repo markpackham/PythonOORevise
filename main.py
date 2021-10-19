bb9e7f0161d77f1e357567b19201ebdaaa194105
mark <markpackham1@gmail.com>
2021-10-19 12:34:58 +0100

    show class name used when getting all attribute of an instance
    https://youtu.be/Ej_02ICOIgs?t=5440

--- main.py
+++ main.py
@@ -62,13 +62,21 @@ class Item:
             return False
 
     def __repr__(self):
-        return f"Item ({self.name},{self.price},{self.quantity})"
+        return f"{self.__class__.__name__} ({self.name},{self.price},{self.quantity})"
 
 
 
 
 class Phone(Item):
-    pass
+    
+    def __init__(self, name: str, price: float, quantity=1, broken_phones=0):
+        # Use super to get parent's abilities
+        super().__init__(
+            name,price,quantity
+        )
+
+        self.broken_phones = broken_phones
+
 
 
 # Item.instantiate_from_csv()
@@ -76,7 +84,9 @@ class Phone(Item):
 
 # print(Item.is_integer(11.0))
 
-phone1 = Item("jscPhonev10", 500,5)
-phone1.broken_phones = 1
-phone2 = Phone("jscPhonev20", 700,5)
-phone2.broken_phones = 1
+phone1 = Phone("jscPhonev10", 500,5,1)
+# print(phone1.calculate_total_price())
+# phone2 = Phone("jscPhonev20", 700,5, 1)
+
+print(Item.all)
+print(Phone.all)
