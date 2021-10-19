104a582b22f81dc46b478bcb233d0ecc3d87810f
mark <markpackham1@gmail.com>
2021-10-19 15:28:31 +0100

    use child class keyboard

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from item import Item
+from keyboard import Keyboard
 
 item1 = Item("MyItem", 750, 6)
+keyboard1 = Keyboard("MyKeyboardItem", 750, 6)
+
+keyboard1.apply_discount()
+print(keyboard1.price)
